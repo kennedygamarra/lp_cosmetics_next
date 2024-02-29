6a5a16b39672810c4635ef9ea15ab572f1f94a41
kennedygamarra <kennedy.gamarra@hypersoft.co>
2024-02-28 21:06:55 -0500

    feat: refactor Header component to include search functionality

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,17 +1,44 @@
+"use client"
+
 import Image from "next/image";
 import logo from "../../../public/image/logo.jpeg";
+import { useEffect, useRef, useState } from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function Header() {
+  const router = useRouter();
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [handleSearching, setHandleSearching] = useState(false);
+
+  const handleButtonSearch = () => {
+    setHandleSearching(!handleSearching);
+  }
+  
+  useEffect(() => {
+    if (inputRef.current?.focus && handleSearching)
+    inputRef.current.focus();
+  }, [handleSearching]);
+
+  const handleSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if(e.key === 'Enter'){
+      //Evitar que se recargue la página
+      e.preventDefault();
+      //Quitar el foco del input/ocultar el input
+      inputRef.current?.blur();
+      if (e.currentTarget.value === "") {
+        router.push("/", { scroll: false });
+        return;
+      }else{
+        router.push(`/search?q=${e.currentTarget.value}`);
+      }
+    }
+  };
+  
   return (
-    <div className="sticky top-0 inset-x-0 z-50 group">
-      <header className="bg-[#fcc4c5] text-[#a86872] p-4 text-center flex justify-center items-center mb-4">
-        <Image src={logo} alt="Logo" className="w-20" />
-        {/* <h1 className="ml-2 text-3xl font-semibold">Catálogo de Productos</h1> */}
-        <form>
-          <label className="mb-2 text-sm font-medium text-gray-900 sr-only ">
-            Search
-          </label>
-          <div className="relative w-fit">
+    <div className="sticky top-0 inset-x-0 z-50 group mb-10">
+      <form >
+          <div className={!handleSearching ? 'hidden' : ''+'absolute z-10 w-full bg-[rgba(30,30,30,.58)] h-screen'}>
             <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
               <svg
                 className="w-4 h-4 text-gray-500"
@@ -32,18 +59,35 @@ export default function Header() {
             <input
               type="search"
               id="default-search"
-              className="block w-[30rem] p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+              className="block w-screen p-4 ps-10 text-sm text-gray-90 bg-gray-50 h-20"
               placeholder="Buscar Productos..."
-              required
+              ref={inputRef}
+              onKeyDown={handleSearch}
+              onBlur={handleButtonSearch}
             />
-            <button
-              type="submit"
-              className="text-white absolute end-2.5 bottom-2.5 hover:bg-[#] focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-lg text-sm px-4 py-2 bg-[#993e50]"
-            >
-              Search
-            </button>
           </div>
         </form>
+      <header className="bg-[#fcc4c5] text-[#a86872] px-4 md:px-20 text-center flex justify-center items-center">
+        <Link href="/" className="w-20 mx-[auto]">
+          <Image src={logo} alt="Logo" className="w-20 mx-[auto]" />
+        </Link>
+        <button onClick={handleButtonSearch}>
+          <svg
+            className="w-6 h-6"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+          </svg>
+        </button>
       </header>
     </div>
   );
