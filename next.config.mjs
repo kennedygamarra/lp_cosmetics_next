c5bc83ee18ae9e18bc24f1b8413a680af8494466
kennedygamarra <kennedy.gamarra@hypersoft.co>
2024-02-28 21:03:08 -0500

    feat: config image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['back.caprichosmakeup.com'],
+  },
+};
 
 export default nextConfig;
