115d6f63ff341212310c7aa05118f6ed1c776098
kennedygamarra <kennedy.gamarra@hypersoft.co>
2024-02-28 21:07:31 -0500

    fix: Catalog component layout

--- src/app/catalog/Catalog.tsx
+++ src/app/catalog/Catalog.tsx
@@ -1,8 +1,6 @@
 import { Product as ProductInterface} from "../types/Product";
 import Product from "./Product";
 
-// import * as products from './data.json';
-
 interface CatalogProps {
   products: ProductInterface[];
 }
@@ -10,8 +8,8 @@ interface CatalogProps {
 export default function Catalog({products}: CatalogProps) {
   return (
     <ul className="flex flex-col gap-x-6">
-      <div className="content-container p-[2px] py-12 small:py-24 ">
-        <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-5 gap-y-5 small:gap-y-36">
+      <div className="content-container px-2 py-12 small:py-24">
+        <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-5 gap-y-5 small:gap-y-36 md:mx-10">
           {/* Generar productos */}
           {products.map((product,index) => (
             <Product key={index} code={product.code} name={product.name} price={product.price} image={product.image} />
