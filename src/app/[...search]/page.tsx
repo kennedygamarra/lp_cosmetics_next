6f810eb15b3385188f8fce4d30b8aae34cf4e8ad
kennedygamarra <kennedy.gamarra@hypersoft.co>
2024-03-10 23:31:09 -0500

    refactor: search functionality and improve code readability

--- src/app/[...search]/page.tsx
+++ src/app/[...search]/page.tsx
@@ -1,43 +1,49 @@
-"use client"
+"use client";
 
-import {useState } from "react";
+import { useState } from "react";
 import { Product as ProductType } from "../types/Product";
-import * as PRODUCT_LIST from '../data.json';
 import Product from "../catalog/Product";
 import { useSearchParams } from "next/navigation";
+import { products } from "../dataUtils";
 
 export default function Search() {
-  const searchParams = useSearchParams()
-  const query = searchParams.get('q');
+  const searchParams = useSearchParams();
+  const query = searchParams.get("q");
 
-  
-  const products: ProductType[] = PRODUCT_LIST.map(item => ({
-    code: item.codpro,
-    name: item.nombre,
-    price: item.precio,
-    image: item.imagen
-  }));
-
-  const productsSearch : any = products.filter((product) => {;
-    return product.name.toLowerCase().includes(query ? query.toLowerCase() : '');
+  const productsSearch: any = products.filter((product) => {
+    return product.name
+      .toLowerCase()
+      .includes(query ? query.toLowerCase() : "");
   });
 
-  const [productList, setProductList] = useState<ProductType[]>(productsSearch);  
-  
+  const [productList, setProductList] = useState<ProductType[]>(productsSearch);
+
   return (
     <>
-      <h1 className="text-3xl font-light text-center my-8 text-gray-700">Resultados de la búsqueda &quot;<span className="font-medium">{query}</span>&quot;</h1>
-      <ul className="flex flex-col gap-x-6">
-        <div className="content-container px-2 py-12 small:py-24">
-          <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-5 gap-y-5 small:gap-y-36 md:mx-10">
-            {/* Generar productos */}
-            {productList.map((product,index) => (
-              <Product key={index} code={product.code} name={product.name} price={product.price} image={product.image} />
-              ))}
+      {query && (
+        <>
+          <h1 className="text-3xl font-light text-center my-8 text-gray-700">
+            Resultados de la búsqueda &quot;
+            <span className="font-medium">{query}</span>&quot;
+          </h1>
+          <ul className="flex flex-col gap-x-6">
+            <div className="content-container px-2 py-12 small:py-24">
+              <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-5 gap-y-5 small:gap-y-36 md:mx-10">
+                {/* Generar productos */}
+                {productList.map((product, index) => (
+                  <Product
+                    key={index}
+                    code={product.code}
+                    name={product.name}
+                    price={product.price}
+                    image={product.image}
+                  />
+                ))}
+              </ul>
+            </div>
           </ul>
-        </div>
-      </ul>
+        </>
+      )}
     </>
-    
   );
 }
