b713167c316f4230982c7f7c3e4f65a2832afed0
kennedygamarra <kennedy.gamarra@hypersoft.co>
2024-03-11 10:28:48 -0500

    refactor: dataUtils.tsx Update price calculation function name and add comments

--- src/app/dataUtils.tsx
+++ src/app/dataUtils.tsx
@@ -4,12 +4,12 @@ import { Product as ProductType } from "./types/Product";
 export const products: ProductType[] = PRODUCT_LIST.map(item => ({
   code: item.codpro,
   name: item.nombre,
-  price: aumentarPrecio(item.precio),
+  price: increasePrice(item.precio),
   image: item.imagen
 }));
 
-//Aumentar precio en 70% 
-function  aumentarPrecio(precio: any) {
+//Up the price of the products
+ function  increasePrice(precio: any) {
   const nuevoPrecio = precio * 1.7;
   // Redondear a la cifra más cercana en cienes
   return Math.round(nuevoPrecio / 100) * 100;
