412470439d13017a82806206a5f3290b208fcb30
kennedygamarra <kennedy.gamarra@hypersoft.co>
2024-03-10 23:30:31 -0500

    refactor: data loading in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,9 @@
 
 import { useState } from "react";
 import Catalog from "./catalog/Catalog";
-import * as PRODUCT_LIST from './data.json';
 import { Product } from "./types/Product";  
 import Search from "./[...search]/page";
-
-const products: Product[] = PRODUCT_LIST.map(item => ({
-  code: item.codpro,
-  name: item.nombre,
-  price: item.precio,
-  image: item.imagen
-}));
-
+import { products } from "./dataUtils";
 
 export default function Home() {
 
